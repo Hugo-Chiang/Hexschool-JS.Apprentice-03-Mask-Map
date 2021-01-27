e93210eea30a8c84c19b91c76f72a57ca124f091
Hugo-Chiang <statesman1991@gmail.com>
2021-01-27 23:48:59 +0800

    21012705update

--- js/all.js
+++ js/all.js
@@ -3,11 +3,16 @@ const sideBar = document.querySelector('#sideBar');
 const collapseButton = document.querySelector('#collapseButton');
 const countySelection = document.querySelector('#countySelection');
 const districtSelection = document.querySelector('#districtSelection');
+const sortButton = document.querySelectorAll('.sortButton');
 let resArr = [];
+let selectedSuppliersArr = [];
 
 // 監聽事件
 collapseButton.addEventListener('click', collapseSideBar);
 countySelection.addEventListener('change', renderDistrictSelection);
+countySelection.addEventListener('change', selectedCounty);
+districtSelection.addEventListener('change', selectedDistrict);
+sortButton.forEach(button => button.addEventListener('click', switchSortButton));
 
 // Leaflet初始佈局
 let backgroudMap = L.map('backgroudMap').setView([25.0238087, 121.5531104], 20);
@@ -44,6 +49,14 @@ xhr.onload = function () {
 
 }
 
+// 函式：側邊攔伸縮
+function collapseSideBar() {
+    let direction = document.querySelector('#direction');
+
+    sideBar.classList.toggle('-show');
+    direction.classList.toggle('-reverse');
+}
+
 // 函式：渲染縣市下拉選單
 function renderCountySelection() {
     for (let i = 0; i < resArr.length; i++) {
@@ -73,22 +86,90 @@ function renderDistrictSelection() {
     }
 }
 
-
-// 函式：側邊攔伸縮
-function collapseSideBar() {
-    let direction = document.querySelector('#direction');
-
-    sideBar.classList.toggle('-show');
-    direction.classList.toggle('-reverse');
+// 函式：已選擇縣市
+function selectedCounty() {
+    filterList.innerHTML = '';
 }
 
-// 函式：選擇縣市
+// 函式：已選擇行政區
+function selectedDistrict() {
+    filterList.innerHTML = '';
 
+    let selectedDistrict = districtSelection.value;
 
-// 函式：選擇行政區
+    selectedSuppliersArr = [];
 
+    for (let i = 0; i < resArr.length; i++) {
+        let town = resArr[i].properties.town;
+
+        if (selectedDistrict == town) {
+            let selectedSupplier = {
+                supplierName: resArr[i].properties.name,
+                supplierAddress: resArr[i].properties.address,
+                supplierPhone: resArr[i].properties.phone,
+                maskAdult: resArr[i].properties.mask_adult,
+                maskChild: resArr[i].properties.mask_child
+            };
+
+            selectedSuppliersArr.push(selectedSupplier);
+        }
+    }
+
+    sortFilterList('不指定', selectedSuppliersArr);
+}
 
 // 函式：切換排序按鈕
+function switchSortButton(e) {
+    e.preventDefault();
 
+    sortButton.forEach((sortButton) => sortButton.classList.remove('selected'));
+    e.target.classList.add('selected');
+
+    sortFilterList(e.target.value, selectedSuppliersArr);
+}
+
+
+// 函式：更新清單排序
+function sortFilterList(sortBasis, suppliersArr) {
+    filterList.innerHTML = '';
+    let arrCopy = suppliersArr.slice();
+
+    switch (sortBasis) {
+        case '不指定':
+            console.log('不指定');
+            arrCopy = suppliersArr;
+            break;
+        case '成人口罩':
+            console.log('成人口罩');
+            arrCopy.sort(function (a, b) {
+                return b.maskAdult - a.maskAdult;
+            });
+            console.log(selectedSuppliersArr);
+            break;
+        case '兒童口罩':
+            console.log('兒童口罩');
+            arrCopy.sort(function (a, b) {
+                return b.maskChild - a.maskChild;
+            });
+            console.log(selectedSuppliersArr);
+            break;
+        default:
+            break;
+    }
+
+    let str = '';
+
+    for (let i = 0; i < arrCopy.length; i++) {
+        str += `
+            <li class="supplier">
+                <h3 class="supplierName">${arrCopy[i].supplierName}</h3>
+                <div class="supplierAddress">${arrCopy[i].supplierAddress}</div>
+                <div class="supplierPhone">${arrCopy[i].supplierPhone}</div>
+                <div>成人口罩${arrCopy[i].maskAdult}個</div>
+                <div>兒童口罩${arrCopy[i].maskChild}個</div>
+            </li>`;
+    }
+    filterList.innerHTML = str;
+}
 
 // 函式：點擊Google導航
