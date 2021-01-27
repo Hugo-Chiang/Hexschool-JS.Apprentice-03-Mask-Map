bb7c04f3768f907953afdd33dd402d199c2758b3
Hugo-Chiang <statesman1991@gmail.com>
2021-01-27 19:40:00 +0800

    21012702update

--- js/all.js
+++ js/all.js
@@ -1,29 +1,28 @@
 // Leaflet初始佈局
-let backgroudMap = L.map('backgroudMap').setView([25.0238087, 121.5531104], 16);
+let backgroudMap = L.map('backgroudMap').setView([25.0238087, 121.5531104], 20);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 }).addTo(backgroudMap);
 
 //Leaflet佈局地圖群組插件（透過新圖層）
-let makers = new L.MarkerClusterGroup().addTo(backgroudMap);
+let markers = new L.MarkerClusterGroup().addTo(backgroudMap);
 
 // XMLHttpRequest初始佈局
 let xhr = new XMLHttpRequest();
 xhr.open('get', 'https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json');
 xhr.send();
+
+// xhr讀取回應檔案後的行為
 xhr.onload = function () {
     let resArr = JSON.parse(xhr.responseText).features;
-    console.log(resArr);
 
     for (let i = 0; i < resArr.length; i++) {
+        markers.addLayer(L.marker([resArr[i].geometry.coordinates[1], resArr[i].geometry.coordinates[0]]).bindPopup(`<h2>${resArr[i].properties.name}</h2>`));
         // if () {
         //     makers.addLayer()
         // } else {
 
         // }
     }
-
+    backgroudMap.addLayer(markers);
 }
-
-// L.marker([25.0238087, 121.5531104]).addTo(backgroudMap).bindPopup('<h2>這是六張犁站</h2>');
-
